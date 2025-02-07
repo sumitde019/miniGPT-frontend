faf613977f7e5c9c951446b33763d38f8888aaef
Sumit De <sumitde24012004@gmail.com>
2025-02-08 00:23:33 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { UserProvider } from "./context/UserContext.jsx";
+import { ChatProvider } from "./context/ChatContext.jsx";
+
+export const server = "https://minigpt.onrender.com";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UserProvider>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </UserProvider>
+  </React.StrictMode>
+);
